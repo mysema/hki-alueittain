89112a144e937ed102d62635c07a87721d490911
jstaffans <johannes.staffans@gmail.com>
2013-11-28 18:50:34 +0200

    Example of clipping layers in leaflet map

--- map-prototype/public/javascripts/main.js
+++ map-prototype/public/javascripts/main.js
@@ -5,54 +5,38 @@ L.tileLayer('http://{s}.tile.cloudmade.com/c3f9f79471e64cd8b7de37c42b2aa752/9862
   maxZoom: 18
 }).addTo(map);
 
-var oldCircles = [
-  L.circle([60.162, 24.945], 500, {
-    color: '#f03',
-    fillColor: '#f03',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.170, 24.92], 900, {
-    color: '#f03',
-    fillColor: '#f03',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.176, 24.97], 300, {
-    color: '#f03',
-    fillColor: '#f03',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.150, 24.98], 400, {
-    color: '#f03',
-    fillColor: '#f03',
-    fillOpacity: 0.3
-  })
-];
+var areas = [{
+  type: 'Feature',
+  properties: {title: 'sqm_per_person'},
+  geometry: {
+    type: 'Polygon',
+    coordinates: [[
+      [24.92, 60.18],
+      [24.95, 60.17],
+      [24.91, 60.15]
+    ]]
+  }
+}];
 
-var newCircles = [
-  L.circle([60.162, 24.945], 400, {
-    color: '#0f3',
-    fillColor: '#0f3',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.170, 24.92], 700, {
-    color: '#0f3',
-    fillColor: '#0f3',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.176, 24.97], 450, {
-    color: '#0f3',
-    fillColor: '#0f3',
-    fillOpacity: 0.3
-  }),
-  L.circle([60.150, 24.98], 600, {
-    color: '#0f3',
-    fillColor: '#0f3',
-    fillOpacity: 0.3
-  })
-];
+L.geoJson(areas, {
+  style: function(feature) {
+    return {
+      color: '#ff0000'
+    }
+  }
+}).addTo(map);
 
-var oldCircleLayer = L.layerGroup(oldCircles);
-var newCircleLayer = L.layerGroup(newCircles);
+console.log(map._layers);
 
-oldCircleLayer.addTo(map);
-newCircleLayer.addTo(map);
+map._layers[24]._container.style.clipPath = 'url(#clipLeft)';
+
+L.geoJson(areas, {
+  style: function(feature) {
+    return {
+      color: '#0000ff'
+    }
+  }
+}).addTo(map);
+
+map._layers[28]._container.style.clipPath = 'url(#clipRight)';
+
