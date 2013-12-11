5337240d7c0db850902b95c1068d8af4a7197cdf
jstaffans <johannes.staffans@gmail.com>
2013-12-11 17:06:45 +0200

    HTML #5

---
+++ resources/public/config.rb
@@ -0,0 +1,25 @@
+# To generate CSS stylesheets from the SASS sources, install Compass and run `compass compile`
+# in this directory.
+
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "styles"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
